98a87cc5bc65a261c1ebb418ba4458662124957a
svinothkumaryoung <vinchi.frnds@gmail.com>
2022-09-27 17:27:26 +0530

    New

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.exceptionHandler.ResourceNotFoundException;
 import com.example.demo.model.Registeration;
 import com.example.demo.services.RegisterationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/home")
@@ -17,6 +21,40 @@ public class HomeController {
     {
         return registerationService.saveStudent(reg);
     }
+
+    @GetMapping("/GetInfo/{id}")
+    public Optional<Registeration> getStudentRecords
+            (@PathVariable (value="id") Integer RegisterNo){
+        return registerationService.getStudentInfo(RegisterNo);
+    }
+
+
+    @GetMapping("GetAllInfo")
+    public List<Registeration> getAllStudentRecords()
+    {
+        return registerationService.getAllStudentInfo();
+
+    }
+    @DeleteMapping("Delete/{id}")
+    public void DeleteRecords
+            (@PathVariable (value = "id") Integer RegisterationNo)
+    {
+         registerationService.deleteStudentRecords(RegisterationNo);
+    }
+
+    @PutMapping("updateRecords/{id}")
+    public Registeration updateRecords
+            (@RequestBody Registeration registeration,
+             @PathVariable (value = "id") Integer id)
+    {
+        Registeration oldRecords=this.registerationService.getStudentInfo(id)
+                .orElseThrow(()->new ResourceNotFoundException("User id Request" +
+                        " Not Exist"));
+        oldRecords.setName(registeration.getName());
+        oldRecords.setDepartment(registeration.getDepartment());
+        return this.registerationService.saveStudent(oldRecords);
+    }
+
 
 
 }
--- src/main/java/com/example/demo/services/RegisterationService.java
+++ src/main/java/com/example/demo/services/RegisterationService.java
@@ -6,16 +6,26 @@ import com.example.demo.repositories.StudentRegisterationRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class RegisterationService {
-
     @Autowired
     StudentRegisterationRepo studentRegisterationRepo;
-
-
-
     public Registeration saveStudent(Registeration registeration){
         return studentRegisterationRepo.save(registeration);
+    }
+    public Optional<Registeration> getStudentInfo(Integer RegisterNo)
+    {
+        return studentRegisterationRepo.findById(RegisterNo);
+    }
+    public List<Registeration> getAllStudentInfo()
+    {
+        return studentRegisterationRepo.findAll();
+    }
+    public void deleteStudentRecords(Integer RegisterationNo){
+         studentRegisterationRepo.deleteById(RegisterationNo);
     }
 
 }
